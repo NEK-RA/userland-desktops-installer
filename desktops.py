2cb98f9acffd63d581c3c6cb82cb4ccdf038efbc
Ryoidenshi Aokigahara <ryoidenshiaokigahara@gmail.com>
2021-06-06 21:28:49 +0300

    twm and icewm info added for test purposes

---
+++ desktops.py
@@ -0,0 +1,22 @@
+twm = dict(
+  packages = ("twm"),
+  xstartup = """
+  #!/bin/sh
+  xrdb $HOME/.Xresources
+  xsetroot -solid grey
+  #x-terminal-emulator -geometry 80x24+10+10 -ls -title "$VNCDESKTOP Desktop" &
+  #x-window-manager &
+  # Fix to make GNOME work
+  export XKL_XMODMAP_DISABLE=1
+  /etc/X11/Xsession
+  """
+)
+
+icewm = dict(
+  packages = ("icewm","xfe"),
+  xstartup = """
+  #!/bin/bash
+  xrdb $HOME/.Xresources
+  icewm-session &
+  """
+)
