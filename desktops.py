538f3830b898f4faf2ebf9b35991ac8667069080
Ryoidenshi Aokigahara <ryoidenshiaokigahara@gmail.com>
2021-06-08 17:49:59 +0300

    logic of remove desktop looks working fine

--- desktops.py
+++ desktops.py
@@ -4,17 +4,18 @@ def shell(cmd,capture=False):
   # by default capture=False to provide shell("command") which will print ot stdout result of execution
   out = run(cmd, shell=True, capture_output=capture)
   if capture:
-    print(out)
     # Not sure if it will work as expected
     # Expected that if stdout is empty then stderr shouln't be empty
     # So if stdout is empty, then stderr should be printed and vice-versa
     return out.stdout.decode("utf-8") or out.stderr.decode("utf-8")
+  else:
+    return out
 
 supported = dict(
-
+  # ~2MB total space usage
   twm = dict(
-    packages2install = ("twm"),
-    packages2remove = ("twm"),
+    packages2install = ["twm"],
+    packages2remove = ["twm"],
     xstartup = """
     #!/bin/sh
     xrdb $HOME/.Xresources
@@ -26,11 +27,11 @@ supported = dict(
     /etc/X11/Xsession
     """
   ),
-
+  # ~30MB total space usage
   icewm = dict(
-    packages2install = ("icewm","xfe"),
+    packages2install = ["icewm","xfe"],
     # not sure if packages2remove will be realy needed, or all other packages (like icewm-common) will be uninstalled on "apt autoremove"
-    packages2remove = ("icewm", "icewm-common","xfe"),
+    packages2remove = ["icewm", "icewm-common","xfe"],
     xstartup = """
     #!/bin/bash
     xrdb $HOME/.Xresources
@@ -53,9 +54,30 @@ def remove(id):
   # TODO: Remove selected de/wm if it's supported
   # TODO: Detect if it realy installer before removing
   if detected(id):
-    print("Removing " + id + "...")
+    print(f"""
+    
+    Removing main packages of {id}...
+
+    WARNING: At the next step script will run next commands for {id} packages:
+    "sudo apt purge" for each main package
+    "sudo apt autoremove -y" after removing main packages to remove also autoinstalled
+
+    You will be asked for sudo password!
+
+    """)
+    canceled = False
+    for pkg in supported[id]["packages2install"]:
+      info = shell("sudo apt purge "+pkg)
+      if info.returncode == 1:
+        canceled = True
+        break
+    if not canceled:
+      shell("sudo apt autoremove -y")
+      print(f"\n\n All packages related to {id} should be removed for now. Restart your UserLAnd session before installing another one desktop!")
+    else:
+      print(f"You canceled uninstallation of {id}")
   else:
-    print("Package "+str(id) + " is not installed! Nothing to remove!")
+    print(f"Package {id} is not installed! Nothing to remove!")
 
 def detected(package):
   # Searching on debian based systems through dpkg. 
@@ -64,7 +86,9 @@ def detected(package):
   # I'm searching exactly "^ii  package-name " which searchs from the line's start 
   # providing few symbols expected for line's start, then 2 spaces, then package name and finally one space to find exactly package-name
   found_package = shell("dpkg -l | grep \"^ii  "+str(package)+" \"",True)
+  # If required package not found then empty string (0 length) will be received, otherwise required package found
+  # TODO: probably it's better to return len(found_package) instead of checking it inside function and return true/false?
   if len(found_package) == 0:
-    return 0
+    return False
   else:
-    return 1
+    return True
