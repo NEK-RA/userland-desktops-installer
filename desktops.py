83e18efec54c568efc7ecc4b7ca76fe20b123c9c
Ryoidenshi Aokigahara <ryoidenshiaokigahara@gmail.com>
2021-06-08 14:11:04 +0300

    detecting packages via dpkg (debian-based) added

--- desktops.py
+++ desktops.py
@@ -1,7 +1,14 @@
+from subprocess import run
+
+def shell(cmd):
+  out = run(cmd, shell=True, capture_output=True).stdout.decode("utf-8")
+  return out
+
 supported = dict(
 
   twm = dict(
-    packages = ("twm"),
+    packages2install = ("twm"),
+    packages2remove = ("twm"),
     xstartup = """
     #!/bin/sh
     xrdb $HOME/.Xresources
@@ -15,7 +22,9 @@ supported = dict(
   ),
 
   icewm = dict(
-    packages = ("icewm","xfe"),
+    packages2install = ("icewm","xfe"),
+    # not sure if packages2remove will be realy needed, or all other packages (like icewm-common) will be uninstalled on "apt autoremove"
+    packages2remove = ("icewm", "icewm-common","xfe"),
     xstartup = """
     #!/bin/bash
     xrdb $HOME/.Xresources
@@ -28,14 +37,28 @@ supported = dict(
 def add(id):
   # TODO: Add selected de/wm if it's supported
   # TODO: Detect if it already installed
-  # detect(id)
-  print("Adding " + id + "...")
+  if not detected(id):
+    print("Adding " + id + "...")
+  else:
+    print("Package "+str(id) + " is already installed! Skipping")
+
 
 def remove(id):
   # TODO: Remove selected de/wm if it's supported
   # TODO: Detect if it realy installer before removing
-  # detect(id)
-  print("Removing " + id + "...")
+  if detected(id):
+    print("Removing " + id + "...")
+  else:
+    print("Package "+str(id) + " is not installed! Nothing to remove!")
 
-def detect(id):
-  print("Detecting " + id)
+def detected(package):
+  # Searching on debian based systems through dpkg. 
+  # Packages installed with "apt install package-name" are listed as "ii  package-name" and some additional info in table-like interface
+  # If I need exactly "package-name" whithout anything like "lib-something-package-name" or "package-name-plugin" and etc. (including substrings from description)
+  # I'm searching exactly "^ii  package-name " which searchs from the line's start 
+  # providing few symbols expected for line's start, then 2 spaces, then package name and finally one space to find exactly package-name
+  found_package = shell("dpkg -l | grep \"^ii  "+str(package)+" \"")
+  if len(found_package) == 0:
+    return 0
+  else:
+    return 1
